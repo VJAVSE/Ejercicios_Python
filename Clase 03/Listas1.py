c1b0b428a18aa98d04dc16a54d01e1e12f0c0112
VJAVSE <v.sereyencina@outlook.com>
2024-05-18 13:58:36 -0400

    Ejercicios de Listas

---
+++ Clase 03/Listas1.py
@@ -0,0 +1,8 @@
+#Escribir un programa que almacene las asignaturas de un curso
+#(por ejemplo  Matemática, Física, Química, Historia y Lengua)
+#en una lista y la muestra por pantalla.
+
+asignaturas= ["Matematicas","Fisica","Quimica","Historia","Lengua"]
+for asignatura in asignaturas:
+    print("Yo estudio: "+asignatura)
+    
---
+++ Clase 03/Listas2.py
@@ -0,0 +1,16 @@
+#Escribir un programa que almacene las asignaturas de un curos
+#por ejemplo Matematicas, Fisica, Quimica, Historia y Lengua
+#en una lista, pregunte al usuario la nota que ha sacado en cada
+#asignatura y despues las muestre por pantalla con el mensaje 
+#En <asignatura> has sacado <nota> donde <asignatura> es cada una
+#de las asignaturas de la lista y <nota> cada una de las correspondientes
+#notas introducidas por el usuario.
+
+asignaturas= ["Matematicas","Fisica","Quimica","Historia","Lengua"]
+notas=[]
+for asignatura in asignaturas:
+    nota=input("¿Qué nota has sacado en: "+asignatura+"? ")
+    notas.append(nota)
+
+for i in range(len(asignaturas)):
+    print("En "+asignaturas[i]+ "has sacado: "+notas[i])
